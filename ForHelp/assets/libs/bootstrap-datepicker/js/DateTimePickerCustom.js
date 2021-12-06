f72883547e6bd3adfc504adab7f7d191a911e45f
Aqib_CodeForPakistan <aqibiqbal@codeforpakistan.org>
2021-12-06 11:55:29 +0500

    ForHelp FOlder

---
+++ ForHelp/assets/libs/bootstrap-datepicker/js/DateTimePickerCustom.js
@@ -0,0 +1,36 @@
+﻿//$(document).ready(function () { 
+//    $(".datetimepicker1").datepicker({
+//        beforeShow: function (input, inst) {
+//            setTimeout(function () {
+//                inst.dpDiv.css({
+//                    top: $(".datetimepicker1").offset().top + 35,
+//                    left: $(".datetimepicker1").offset().left
+//                });
+//            }, 0);
+//        }
+//    }); 
+
+
+//    $("input").focus(function () {
+//        $('html, body').animate({ scrollTop: $(this).offset().top - 25 }, 10);
+//    });
+//});
+
+$(document).ready(function () {
+    // Set Datepicker
+    $(".datetimepicker1").datepicker({
+        beforeShow: function (input, inst) {
+            setTimeout(function () {
+                inst.dpDiv.css({
+                    top: $(".datetimepicker1").offset().top + 35,
+                    left: $(".datetimepicker1").offset().left
+                });
+            }, 0);
+        }
+    });
+
+    $("input").focus(function () {
+        $('html, body').animate({ scrollTop: $(this).offset().top - 25 }, 10);
+    });
+
+});
